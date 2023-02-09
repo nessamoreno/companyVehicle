a02dcd1f70a14b3deda1a28cf3a70ea60e36dd67
nessa26 <vanessa.coex@gmail.com>
2023-02-09 11:05:37 -0500

    se creo la clase de empleado lista (EmployeeList)en el archivo views

--- companyCar/views.py
+++ companyCar/views.py
@@ -5,32 +5,31 @@ from django.shortcuts import render
 # from django.views import *
 # from django.http.response import JsonResponse
 # from django.http.request import JsonRequest
-from .models import Vehicle,Client,Employee
 from django.views import generic
 from .models import *
 from django.db.models import F
 # Create your views here.
 
-class Vehicle(generic.ListView):
+class VehicleList(generic.ListView):
     model = Vehicle
     template_name = 'vehicle.html'
 
     def get_context_data(self, **kwargs):
-        context = super(Vehicle, self).get_context_data(**kwargs)
+        context = super(VehicleList, self).get_context_data(**kwargs)
         vehicles = Vehicle.objects.all().annotate(vehicle_type = F('veh_typ_id__type')).annotate(vehicle_price= F('veh_typ_id__price')).values()
         context['vehicles'] = vehicles
 
         return context
     
-class Employee(generic.ListView):
+class EmployeeList(generic.ListView):
     model = Employee
     template_name = 'employee.html'
 
     def get_context_data(self, **kwargs):
-        context = super(Employee, self).get_context_data(**kwargs)
+        context = super(EmployeeList, self).get_context_data(**kwargs)
         employee = Employee.objects.all().values()
         context['employee'] = employee
-        
+
         return context
 
 class Client():
